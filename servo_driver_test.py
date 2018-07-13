8f5fb6e8dfcdd5f4f07350bcab53680a9ad4bbf6
VidyaPuri <vid.cresnik@gmail.com>
2018-07-13 21:38:12 +0200

    wrong way

--- servo_driver_test.py
+++ servo_driver_test.py
@@ -7,6 +7,12 @@ servo_min = 125
 servo_max = 540
 pos4  = 300
 
+def set_servo4(a):
+    print("Servo1: ",a)
+    PCA9685_pwm.set_pwm(4,0,a)
+    return "Servo4:",a
+
+
 while True:
     pos4=servo_max
     set_servo4(pos4)
@@ -15,8 +21,3 @@ while True:
     pos4=servo_min
     set_servo4(pos4)
     print("min")
-
-def set_servo4(a):
-    print("Servo1: ",a)
-    PCA9685_pwm.set_pwm(4,0,a)
-    return "Servo4:",a
