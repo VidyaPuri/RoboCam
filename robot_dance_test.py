b0baeaa3478fb370b022259cd391414fdaa0b383
VidyaPuri <vid.cresnik@gmail.com>
2018-07-14 01:33:43 +0200

    robot_dance_test mapped input values

--- robot_dance_test.py
+++ robot_dance_test.py
@@ -30,16 +30,24 @@ def pos_fun(servo):
     while True:
         try:
             b = int(input())
-            if servo_min[servo] <= b <= servo_max[servo]:
+            #if servo_min[servo] <= b <= servo_max[servo]:
+            if 0 <= b <= 180:
                 pos = b
                 print("Position is:",pos)
                 break
             else:
-                print("wrong input, enter number between",servo_min[servo], "and",servo_max[servo])
+                #print("wrong input, enter number between",servo_min[servo], "and",servo_max[servo])
+                print("wrong input, enter number between 0° and 180°")
         except ValueError:
             print("invalid input. Value must be integer")
     print("Servo number",servo,"moves to position",pos)
-    move_servo(servo,pos)
+    map(servo,pos)
+
+def map(servo,pos):
+    raw_pos = interp(pos,[0,180],[servo_min[servo],servo_max[servo]])
+    print("servo_min:",servo_min[servo],"servo_max:",servo_max[servo],"deg_input",pos)
+    print("raw value for servos",round(raw_pos,0))
+    move_servo()
 
 def move_servo(servo,pos):
         print("Servo numb:",servo,"moves for:",pos)
