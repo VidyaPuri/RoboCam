cf1634a951c3b3d59db0390aca058a58492b7ea9
VidyaPuri <vid.cresnik@gmail.com>
2018-07-13 21:28:29 +0200

    blablaba test12

--- servo_driver_test.py
+++ servo_driver_test.py
@@ -6,7 +6,7 @@ PCA9685_pwm.set_pwm_freq(60)
 servo_min = 125
 servo_max = 540
 pos4  = 300
-"""
+
 def main():
     set_servo4(pos4)
     print("over")
@@ -15,5 +15,6 @@ def set_servo4(a):
     print("Servo1: ",a)
     PCA.set_pwm(4,0,a)
     return "Servo4:",a
-"""
-print("wakaw")
+
+if __name__ == '__main__':
+    main()
