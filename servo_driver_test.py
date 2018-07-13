b430a9f5271a5c26935577d3888da03d34cb45dd
VidyaPuri <vid.cresnik@gmail.com>
2018-07-13 21:36:34 +0200

    turn to both limits forever

--- servo_driver_test.py
+++ servo_driver_test.py
@@ -7,14 +7,16 @@ servo_min = 125
 servo_max = 540
 pos4  = 300
 
-def main():
+while True:
+    pos4=servo_max
     set_servo4(pos4)
-    print("over")
+    print("max")
+    time.sleep(100)
+    pos4=servo_min
+    set_servo4(pos4)
+    print("min")
 
 def set_servo4(a):
     print("Servo1: ",a)
     PCA9685_pwm.set_pwm(4,0,a)
     return "Servo4:",a
-
-if __name__ == '__main__':
-    main()
